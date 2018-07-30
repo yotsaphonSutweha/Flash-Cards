6a9ab69af10960271c6e96b75093b8151b296502
yotsaphonSutweha <komsan_58@hotmail.com>
2018-07-30 22:50:22 +0100

    set up app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.set('view engine', 'pug');
+
+app.listen(8080, () => {
+    console.log("The app is running at localhost: 8080");
+})
